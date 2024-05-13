de33c1bcde002447a35829070fc43db7a90b81b5
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-05-14 01:04:50 +0900

    Update Landing Page

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 import streamlit as st
-import boto3
-import cmn_settings
-import json
-import logging
 import cmn_auth
-#import pyperclip
-
-
-from botocore.exceptions import ClientError
-
-AWS_REGION = cmn_settings.AWS_REGION
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
 
 st.set_page_config(
     page_title="Chat Application",
     page_icon=":coffee:", #"🧊",
-    layout="centered", #wide
+    layout="wide", #centered
     initial_sidebar_state="expanded",
     menu_items={
         'Get Help': 'https://localhost.com/help',
@@ -26,7 +13,6 @@ st.set_page_config(
     }
 )
 
-
 ###### AUTH START #####
 
 if not cmn_auth.check_password():
@@ -34,130 +20,37 @@ if not cmn_auth.check_password():
 
 ######  AUTH END #####
 
-#def on_copy_click(text):
-#    pyperclip.copy(text)
+tab1, tab2, tab3 = st.tabs([":blue[**Get Started**]", "Prompt Guide", "Links"])
 
-opt_model_id_list = [
-    "anthropic.claude-3-sonnet-20240229-v1:0",
-    "anthropic.claude-3-haiku-20240307-v1:0"
-]
+with tab1:
+    st.header("Get Started")
+    col1, col2, col3 = st.columns(3)
 
-with st.sidebar:
-    opt_model_id = st.selectbox(label="Model ID", options=opt_model_id_list, index = 0, key="model_id")
-    opt_temperature = st.slider(label="Temperature", min_value=0.0, max_value=1.0, value=0.1, step=0.1, key="temperature")
-    opt_top_p = st.slider(label="Top P", min_value=0.0, max_value=1.0, value=1.0, step=0.1, key="top_p")
-    opt_top_k = st.slider(label="Top K", min_value=0, max_value=500, value=250, step=1, key="top_k")
-    opt_max_tokens = st.slider(label="Max Tokens", min_value=0, max_value=4096, value=2048, step=1, key="max_tokens")
-    #opt_system_msg = st.text_area(label="System Message", value="", key="system_msg")
+    with col1:
+        st.header("Chat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
 
-bedrock_runtime = boto3.client('bedrock-runtime', region_name=AWS_REGION)
+    with col2:
+        st.header("Knowlege Base")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
 
-st.title("💬 Chatbot")
+    with col3:
+        st.header("Agents")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+   
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+with tab2:
+    st.header("Prompt Guide")
+    #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.subheader("Text to Image - Example 1")
+    st.markdown(":blue[**prompt:**] breathtaking night street of Tokyo, neon lights. award-winning, professional, highly detailed")
+    st.markdown(":red[**negative:**] anime, cartoon, graphic, text, painting, crayon, graphite, abstract glitch, blurry")
 
-#count = 0
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-    #if msg["role"] == "assistant":
-    #    st.button("📋", on_click=on_copy_click, args=(msg["content"],), key=count)
-    #    count += 1
+with tab3:
+    st.header("Useful Links")
+    #st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.markdown("- :blue-background[name] https://foo/bar.com")
 
-if prompt := st.chat_input():
-
-    message_history = st.session_state.messages.copy()
-    message_history.append({"role": "user", "content": prompt})
-    #st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    #user_message =  {"role": "user", "content": f"{prompt}"}
-    #messages = [st.session_state.messages]
-    print(f"messages={st.session_state.messages}")
-
-    request = {
-        "anthropic_version": "bedrock-2023-05-31",
-        "temperature": opt_temperature,
-        "top_p": opt_top_p,
-        "top_k": opt_top_k,
-        "max_tokens": opt_max_tokens,
-        #"system": opt_system_msg,
-        "messages": message_history
-    }
-    json.dumps(request, indent=3)
-
-    try:
-        response = bedrock_runtime.invoke_model_with_response_stream(
-            modelId = opt_model_id, #bedrock_model_id, 
-            contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
-            accept = "application/json",
-            body = json.dumps(request))
-
-        result_text = ""
-        with st.chat_message("assistant"):
-            result_container = st.container(border=True)
-            result_area = st.empty()
-            stream = response["body"]
-                
-            for event in stream:
-                
-                if event["chunk"]:
-
-                    chunk = json.loads(event["chunk"]["bytes"])
-
-                    if chunk['type'] == 'message_start':
-                        opts = f"| temperature={opt_temperature} top_p={opt_top_p} top_k={opt_top_k} max_tokens={opt_max_tokens}"
-                        #result_container.write(opts)
-
-                    elif chunk['type'] == 'message_delta':
-                        pass
-
-                    elif chunk['type'] == 'content_block_delta':
-                        if chunk['delta']['type'] == 'text_delta':
-                            text = chunk['delta']['text']
-                            result_text += f"{text}"
-                            result_area.markdown(result_text)
-
-                    elif chunk['type'] == 'message_stop':
-                        invocation_metrics = chunk['amazon-bedrock-invocationMetrics']
-                        input_token_count = invocation_metrics["inputTokenCount"]
-                        output_token_count = invocation_metrics["outputTokenCount"]
-                        latency = invocation_metrics["invocationLatency"]
-                        lag = invocation_metrics["firstByteLatency"]
-                        stats = f"| token.in={input_token_count} token.out={output_token_count} latency={latency} lag={lag}"
-                        #result_container.write(stats)
-
-                elif event["internalServerException"]:
-                    exception = event["internalServerException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["modelStreamErrorException"]:
-                    exception = event["modelStreamErrorException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["modelTimeoutException"]:
-                    exception = event["modelTimeoutException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["throttlingException"]:
-                    exception = event["throttlingException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["validationException"]:
-                    exception = event["validationException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                else:
-                    result_text += f"\n\nUnknown Token"
-                    result_area.write(result_text)
-
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.session_state.messages.append({"role": "assistant", "content": result_text})
-
-        #st.button("📋", on_click=on_copy_click, args=(result_text,))
-        
-    except ClientError as err:
-        message = err.response["Error"]["Message"]
-        logger.error("A client error occurred: %s", message)
-        print("A client error occured: " + format(message))
-        st.chat_message("system").write(message)
+    st.markdown("> blockquote")
+    st.markdown("- [x] Write the press release") 
+    st.markdown("- [ ] Write the press release  H~2~O X^2^") 
